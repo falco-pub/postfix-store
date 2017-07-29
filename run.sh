97af370518bd4c9670d1ea08bbc14f584d51c7e3
Falco <falco@falco.bz>
2017-07-29 02:57:33 +0200

    improve run.sh wrt postmap

--- run.sh
+++ run.sh
@@ -4,10 +4,15 @@ touch /var/log/maillog
 touch /var/log/messages
 chmod o+r /var/log/*
 
-postmap /etc/postfix/user/virtual
+pushd /etc/postfix/user/
+for f in `find -type f`; do
+    c=`awk '/^#run:/ {gsub (/^#run:/,""); print $0}' $f`
+    [ -z "$c" ] || ( echo + $c $f && eval $c $f )
+done
+popd
 
-chmod 1777 /var/spool/mail 
-mkdir /var/spool/mail/.logs     
+chmod 1777 /var/spool/mail
+mkdir /var/spool/mail/.logs
 chmod 1777 /var/spool/mail/.logs
 mkdir /var/spool/mail/nobody
 chown nobody: /var/spool/mail/nobody
