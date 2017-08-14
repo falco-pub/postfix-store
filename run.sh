b20f5d33d1a755ab398ccfe62546d4c4705af15f
Falco <falco@falco.bz>
2017-08-14 13:25:46 +0200

    fix path

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ touch /var/log/maillog
 touch /var/log/messages
 chmod o+r /var/log/*
 
-pushd /etc/postfix/user/
+pushd /etc/postfix/
 for f in `find -type f`; do
     c=`awk '/^#run:/ {gsub (/^#run:/,""); print $0}' $f`
     [ -z "$c" ] || ( echo + $c $f && eval $c $f )
